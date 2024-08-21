598db5eb2d75cea6cb974e3f6943a1c8f69d4527
caferven <carol96lorac@gmail.com>
2024-08-21 16:34:39 +0200

    add user registration

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
