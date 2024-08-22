a5d57f087da56c2cf0c0c7c7a9d47a07d4b37b56
caferven <carol96lorac@gmail.com>
2024-08-22 11:04:14 +0200

    add profile page and restrictions

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -20,3 +21,7 @@ def register(request):
 def user_logout(request):
     logout(request)
     return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
