a89483392196d952f496b75a6320d3941cc5a166
caferven <carol96lorac@gmail.com>
2024-08-23 16:27:27 +0200

    add model translations and fix my posts

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(_("Title"), max_length=100)
+    content = models.TextField(_("Content"))
     date_posted = models.DateTimeField(default=timezone.now)
     last_modified = models.DateTimeField(auto_now=True)
 
--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default_icon.png', upload_to='profile_pics')
+    image = models.ImageField(_("Image"), default='default_icon.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
